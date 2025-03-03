df127c5091ade9b8fbdbd038b24a514d84bd1dc5
treska03 <bartlomiej.treska@gmail.com>
2025-03-03 22:52:38 +0100

    Change config

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,7 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.7.0")
+    implementation("org.apache.commons:commons-csv:1.13.0")
 }
 
 kotlin {
