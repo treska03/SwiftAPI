ea9aea11b74209b58207b6a4e52d7b7a7ee31549
treska03 <bartlomiej.treska@gmail.com>
2025-03-04 14:32:09 +0100

    Add response status code

--- src/main/kotlin/io/github/treska/swiftapi/exception/SwiftCodesCSVNotFoundException.kt
+++ src/main/kotlin/io/github/treska/swiftapi/exception/SwiftCodesCSVNotFoundException.kt
@@ -1,7 +1,10 @@
 package io.github.treska.swiftapi.exception
 
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
 import java.io.FileNotFoundException
 
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 data class SwiftCodesCSVNotFoundException(
     override val message: String = "swift_codes.csv not found in resources.",
 ) : FileNotFoundException()
